eb21b7f34b546d58375e3d6205a95964f0de6d4a
Html-CSS <shakeel345sh5@gmail.com>
2025-05-19 21:46:20 +0500

    un comment from try code

--- try.cpp
+++ try.cpp
@@ -1,50 +1,52 @@
-// #include<iostream>
-// using namespace std;
+// First Program that mam writes on board
+#include<iostream>
+using namespace std;
 
-// int main (){
-//     int num1 , num2, result;
-//     try
-//     {
-//         cout<<"Enter two numbers";
-//         cin>>num1>>num2;
-//         result = num1 / num2;
-//         cout<<"Result is: "<<result;
-//     }
-//     catch(int no)
-//     {
-//         cout<<"you can not divide by zero";
+int main (){
+    int num1 , num2, result;
+    try
+    {
+        cout<<"Enter two numbers";
+        cin>>num1>>num2;
+        result = num1 / num2;
+        cout<<"Result is: "<<result;
+    }
+    catch(int no)
+    {
+        cout<<"you can not divide by zero";
     
-//     return 0;
-// }
-// }
+    return 0;
+}
+}
 
-// #include<iostream>
-// using namespace std;
+//Second program of Exception handling that mam write on board
+#include<iostream>
+using namespace std;
 
-// int main (){
-//     try
-//     {
-//         cout<<"Enter two numbers";
-//         int num1 , num2, result;
-//         cin>>num1>>num2;
-//         if(num2 != 0){
+int main (){
+    try
+    {
+        cout<<"Enter two numbers";
+        int num1 , num2, result;
+        cin>>num1>>num2;
+        if(num2 != 0){
 
-//             result = num1 / num2;
-//             cout<<"Result is: "<<result;
-//         }
-//         else{
-//             throw(num2);
-//         }
-//     }
-//     catch(int no)
-//     {
-//         cout<<"you can not divide by zero";
+            result = num1 / num2;
+            cout<<"Result is: "<<result;
+        }
+        else{
+            throw(num2);
+        }
+    }
+    catch(int no)
+    {
+        cout<<"you can not divide by zero";
     
-//     return 0;
-// }
-// }
+    return 0;
+}
+}
 
-
+//Third Program that I wrote
 #include <iostream>
 using namespace std;
 int main() {
